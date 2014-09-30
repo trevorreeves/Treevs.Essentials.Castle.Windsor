38b2153fba2c914ff22ea76f8d38d1bcf74345f8
Trevor Reeves <trevorreeves@gmail.com>
2014-09-30 12:08:15 +0100

    Creating nuget package

--- src/Treevs.Essentials.Castle.Windsor/Properties/AssemblyInfo.cs
+++ src/Treevs.Essentials.Castle.Windsor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("treeves.essentials.castle.windsor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Treevs.Essentials.Castle.Windsor")]
+[assembly: AssemblyDescription("Little additions to CastleWindsor that I wouldn't ever leave the house without.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("treeves.essentials.castle.windsor")]
+[assembly: AssemblyProduct("Treevs.Essentials.Castle.Windsor")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
